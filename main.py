22f4816e38bed7f93c9cb81b90b634f2364f9f59
akayami301-ctrl <akayami301@gmail.com>
2026-07-02 17:12:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import itertools
+
+chars = "abcdefghijklmnopqrstuvwxyz0123456789_."
+with open("5c_list.txt", "w") as f:
+    for combo in itertools.product(chars, repeat=5):
+        f.write("".join(combo) + "\n")
+
+print("la list 5c a été gen avec succès mais attention tu a besion d'un checker pour les check du coup...")
